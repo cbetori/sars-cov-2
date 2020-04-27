0ee83757c218ce5caeeae73c6acad2e9e617db39
cbetori <cbetori@gmail.com>
2020-04-27 16:59:40 -0500

    Add build

---
+++ client/build/precache-manifest.bdb74bd8b54a537cf277f1ff50b44eef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45160bc043d163e184700677e12ee62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90b6d0fcd63cbe3e37ff",
+    "url": "/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "129f4ef32c965d082063",
+    "url": "/static/css/main.b0d5bab5.chunk.css"
+  },
+  {
+    "revision": "90b6d0fcd63cbe3e37ff",
+    "url": "/static/js/2.7b0d132c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7b0d132c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129f4ef32c965d082063",
+    "url": "/static/js/main.cad70fd1.chunk.js"
+  },
+  {
+    "revision": "24852033628ed9769a90",
+    "url": "/static/js/runtime-main.0aba8a15.js"
+  }
+]);
